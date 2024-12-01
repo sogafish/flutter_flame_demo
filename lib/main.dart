19f47621ea5cda28eca2b652e331f32302610718
sogafish <kent.soga@gmail.com>
2024-12-01 17:29:32 +0900

    :sparkles: Add PauseMenu

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flame_demo/rikishi_game.dart';
 
 void main() {
-  final game = RikishiGame();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget(
+    game: RikishiGame(),
+    overlayBuilderMap: {
+      'PauseMenu': (context, game) {
+        return Container(
+          color: const Color(0xFF000000),
+          child: const Text('A pause menu'),
+        );
+      },
+    },
+  ));
 }
