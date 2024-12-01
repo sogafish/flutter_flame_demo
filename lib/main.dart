a0edb6bf61751f0c46962b8819e8799f6ff39cf3
sogafish <kent.soga@gmail.com>
2024-12-01 17:00:05 +0900

    :construction: Set GameWidget on Root

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlameDemo extends StatelessWidget {
+  const FlameDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,22 +42,20 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          onPressed: () {}, tooltip: 'Increment', child: const Text('P')),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
     );
   }
 }
