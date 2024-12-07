b741d0ae701c1fefe2f9b67af7fb7c7150f277f3
sogafish <kent.soga@gmail.com>
2024-12-07 18:15:27 +0900

    :sparkles: Fix Scaffolding on KlondikeGame

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_flame_demo/rikishi_game.dart';
+import 'package:flutter_flame_demo/games/klondike_game.dart';
 
 void main() {
-  runApp(GameWidget(
-    game: RikishiGame(),
-    overlayBuilderMap: {
-      'PauseMenu': (context, game) {
-        return Container(
-          color: const Color(0xFF000000),
-          child: const Text('A pause menu'),
-        );
+  runApp(
+    GameWidget(
+      game: KlondikeGame(),
+      overlayBuilderMap: {
+        'PauseMenu': (context, game) {
+          return Container(
+            color: const Color(0xFF000000),
+            child: const Text('A pause menu'),
+          );
+        },
       },
-    },
-  ));
+    ),
+  );
 }
