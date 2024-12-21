6d615823c30e29dbbc695e9c9b890e5305b4a768
sogafish <kent.soga@gmail.com>
2024-12-21 14:51:53 +0900

    Create StockPileComponent

---
+++ lib/components/stock_pile.dart
@@ -0,0 +1,16 @@
+import 'package:flame/components.dart';
+import 'package:flutter_flame_demo/components/card.dart';
+import 'package:flutter_flame_demo/games/klondike_game.dart';
+
+class StockPile extends PositionComponent {
+  StockPile({super.position}) : super(size: KlondikeGame.cardSize);
+
+  final List<Card> _cards = [];
+
+  void acquireCard(Card card) {
+    assert(!card.isFaceUp);
+    card.position = position;
+    card.priority = _cards.length;
+    _cards.add(card);
+  }
+}
