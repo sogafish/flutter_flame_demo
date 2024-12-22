dcdeacd0e76d6fd0780d280600ed290df24d2ff0
sogafish <kent.soga@gmail.com>
2024-12-22 14:17:50 +0900

    Add onTapUp into StockPile

--- lib/components/stock_pile.dart
+++ lib/components/stock_pile.dart
@@ -1,5 +1,7 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flutter_flame_demo/components/card.dart';
+import 'package:flutter_flame_demo/components/waste_pile.dart';
 import 'package:flutter_flame_demo/games/klondike_game.dart';
 
 class StockPile extends PositionComponent {
@@ -12,5 +14,23 @@ class StockPile extends PositionComponent {
     card.position = position;
     card.priority = _cards.length;
     _cards.add(card);
+  }
+
+  void onTapUp(TapUpEvent event) {
+    final wastePile = parent!.firstChild<WastePile>()!;
+    if (_cards.isEmpty) {
+      wastePile.removeAllCards().reversed.forEach((card) {
+        card.flip();
+        acquireCard(card);
+      });
+    } else {
+      for (var i = 0; i < 3; i++) {
+        if (_cards.isNotEmpty) {
+          final card = _cards.removeLast();
+          card.flip();
+          wastePile.acquireCard(card);
+        }
+      }
+    }
   }
 }
