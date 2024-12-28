de7f3fb4871a6c83dc4477f0d839c13539105ddb
sogafish <kent.soga@gmail.com>
2024-12-28 13:55:39 +0900

    Fix FoundationPile

--- lib/components/foundateion_pile.dart
+++ lib/components/foundateion_pile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flutter_flame_demo/components/card.dart';
 import 'package:flutter_flame_demo/games/klondike_game.dart';
@@ -17,5 +19,25 @@ class FoundationPile extends PositionComponent {
     card.position = position;
     card.priority = _cards.length;
     _cards.add(card);
+  }
+
+  final _borderPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10
+    ..color = const Color(0x50ffffff);
+  late final _suitPaint = Paint()
+    ..color = suit.isRed ? const Color(0x3a000000) : const Color(0x64000000)
+    ..blendMode = BlendMode.luminosity;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRRect(KlondikeGame.cardRRect, _borderPaint);
+    suit.sprite.render(
+      canvas,
+      position: size / 2,
+      anchor: Anchor.center,
+      size: Vector2.all(KlondikeGame.cardWidth * 0.6),
+      overridePaint: _suitPaint,
+    );
   }
 }
